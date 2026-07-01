c45b0e81def75814c8d6418340c6006557d76759
GDias <guilhermevsrobin@gmail.com>
2026-07-01 14:33:48 -0300

    Alterações de correção com o feedback do professor fabio

--- cypress/e2e/end-to-end.cy.js
+++ cypress/e2e/end-to-end.cy.js
@@ -7,9 +7,7 @@ describe('Testes End To End do fluxo de cadastro e login', () => {
 
     beforeEach(() => {
         cadastroPage.visitarPaginaCadastro()
-
     });
-
 
     it('Deve fazer o cadastro e validar o login com o usuário cadastrado)', () => {
         cadastroPage.preencherCadastro('Guilherme', 'guilherme@teste.com', '34998101476', 'senha123', 'senha123')
